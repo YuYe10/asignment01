43891e7b4d71fbd8387815fbbac44f23a3aa66c7
yuye <3238918486@qq.com>
2024-10-11 21:46:39 +0800

    deploy environment and program check_dps

--- 1-count/check_dps.py
+++ 1-count/check_dps.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 
 # Step2: Define the remote `get_host` function
+@ray.remote
 def get_host():
     time.sleep(0.001)
     return socket.gethostbyname(socket.gethostname())
@@ -20,11 +21,11 @@ if __name__ == "__main__":
     client_port = os.getenv("CLIENT_PORT")
 
     # Step1: Connect to the Ray cluster
-
+    ray.init(address=f"auto", _temp_dir=None, ignore_reinit_error=True)
     # Step3: Generate a list with 100 `get_host` tasks
-
+    tasks = [get_host.remote() for _ in range(100)]
     # Step4: Get the IP addresses of the machines that executed the tasks
-    ip_addresses = []
+    ip_addresses = ray.get(tasks)
 
     print("Tasks executed")
     for ip_address, num_tasks in Counter(ip_addresses).items():
